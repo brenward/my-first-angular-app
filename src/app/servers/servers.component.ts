45620fb0e59122fa6d7862eafe9f4c5f6cdab93f
brenward <bren.ward@gmail.com>
2024-03-10 14:02:40 +0000

    #Alternate selector options

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //selector: '[app-servers]' -- add <div app-servers></div> to html to select via attribute,
+  //selector: '.app-servers' -- add <div class="app-servers"></div> to html to select via class,
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
