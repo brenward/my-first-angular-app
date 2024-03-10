814aaf21fb87438b82b97aaeb035c1dd0ab7a55b
brenward <bren.ward@gmail.com>
2024-03-10 19:27:38 +0000

    #Event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit{
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
 
   constructor(){
     setTimeout(() => {
@@ -18,6 +19,10 @@ export class ServersComponent implements OnInit{
   
   ngOnInit(): void {
     throw new Error('Method not implemented.');
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created!';
   }
 
 }
