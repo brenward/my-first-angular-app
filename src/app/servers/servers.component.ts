c1dd29a9cbe91fda472e87de158178987e4a6a90
brenward <bren.ward@gmail.com>
2024-03-10 19:22:25 +0000

    #String interpolation {{stringResolvableVariable}} and property binding [attribute]

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000)
+  }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
