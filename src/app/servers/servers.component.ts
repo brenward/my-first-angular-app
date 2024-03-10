6bd52adab815bed65900138cd5729c5087963f5b
brenward <bren.ward@gmail.com>
2024-03-10 20:36:20 +0000

    #ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit{
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
 
   constructor(){
     setTimeout(() => {
@@ -24,10 +25,12 @@ export class ServersComponent implements OnInit{
 
   onCreateServer(){
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: any){
     this.serverName = (<HTMLInputElement>event.target).value;
+    
     
     console.log(event);
   }
