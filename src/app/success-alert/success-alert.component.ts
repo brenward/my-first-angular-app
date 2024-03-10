8ffece77ee43d90817933238e1fa17c41ac4b661
brenward <bren.ward@gmail.com>
2024-03-10 19:47:21 +0000

    #Combining data binding techniques

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -6,5 +6,9 @@ import { Component } from "@angular/core";
     styles:`p{color:green}`
 })
 export class SuccessAlertComponent{
+    username: string='';
 
+    resetUserName(){
+        this.username='';
+    }
 }
